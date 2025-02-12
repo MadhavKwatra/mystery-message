0f3556b249f0702314d1f834a5ef4fc8be37170d
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2025-02-13 01:05:39 +0530

    Updated navbar with dropdown menu, added links to some pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,13 +5,41 @@ import { User } from "next-auth";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { useEffect, useState } from "react";
-import { Moon, Sun, Menu, X } from "lucide-react";
+import {
+  Moon,
+  Sun,
+  Menu,
+  X,
+  Settings,
+  UserRound,
+  LayoutDashboard,
+  LogOut,
+} from "lucide-react";
 import { APP_NAME } from "@/config/config";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { getOptimizedAvatarImageUrl } from "@/lib/utils";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
+  const router = useRouter();
   const { data: session } = useSession();
   const user: User = session?.user as User;
 
+  // Get the avatar URL from the session otherwise set to null
+  let avatar_uri = session?.user.avatar_url || null;
+
+  // optimize the image using cloudinary
+  if (avatar_uri) {
+    avatar_uri = getOptimizedAvatarImageUrl(avatar_uri);
+  }
   const [isDarkMode, setIsDarkMode] = useState(() => false);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -34,7 +62,6 @@ function Navbar() {
       localStorage.setItem("darkMode", "false");
     }
   }, [isDarkMode]);
-
   // Prevent background scroll when menu is open
   useEffect(() => {
     if (mobileMenuOpen) {
@@ -74,36 +101,87 @@ function Navbar() {
               <span className="mr-4 font-bold">
                 Welcome, {user?.username || user?.email}
               </span>
-              <Button
-                className="w-full md:w-auto bg-slate-100 text-black"
-                variant="outline"
-                onClick={() => signOut()}
-              >
-                Logout
-              </Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Avatar className="w-10 h-10 border  border-gray-300 dark:border-gray-700">
+                    {avatar_uri ? (
+                      <>
+                        <AvatarImage
+                          src={avatar_uri}
+                          className="object-cover"
+                          alt="Profile Picture"
+                        />
+                        <AvatarFallback>
+                          {user?.username?.charAt(0) || "U"}
+                        </AvatarFallback>
+                      </>
+                    ) : (
+                      <>
+                        <UserRound className="w-full h-full" />
+                      </>
+                    )}
+                  </Avatar>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-48">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem
+                    onClick={() => {
+                      router.push("/profile/settings");
+                    }}
+                    className="cursor-pointer"
+                  >
+                    <Settings />
+                    Edit Profile
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => {
+                      router.push("/dashboard");
+                    }}
+                    className="cursor-pointer"
+                  >
+                    <LayoutDashboard />
+                    <span>Dashboard</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="cursor-pointer"
+                    onClick={toggleDarkMode}
+                  >
+                    {isDarkMode ? <Moon /> : <Sun />} Change Theme
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="cursor-pointer"
+                    onClick={() => signOut()}
+                  >
+                    <LogOut /> <span>Logout</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             </>
           ) : (
-            <Link href="/sign-in">
+            <>
+              <Link href="/sign-in">
+                <Button
+                  className="w-full md:w-auto bg-slate-100 text-black"
+                  variant={"outline"}
+                >
+                  Login
+                </Button>
+              </Link>
               <Button
-                className="w-full md:w-auto bg-slate-100 text-black"
+                onClick={toggleDarkMode}
+                className=" rounded-full transition-colors bg-slate-100 group"
+                size={"icon"}
                 variant={"outline"}
               >
-                Login
+                {isDarkMode ? (
+                  <Moon className="w-5 h-5 text-gray-800 fill-current group-hover:text-accent-foreground" />
+                ) : (
+                  <Sun className="w-5 h-5 text-gray-800 fill-current" />
+                )}
               </Button>
-            </Link>
+            </>
           )}
-          <Button
-            onClick={toggleDarkMode}
-            className=" rounded-full transition-colors bg-slate-100 group"
-            size={"icon"}
-            variant={"outline"}
-          >
-            {isDarkMode ? (
-              <Moon className="w-5 h-5 text-gray-800 fill-current group-hover:text-accent-foreground" />
-            ) : (
-              <Sun className="w-5 h-5 text-gray-800 fill-current" />
-            )}
-          </Button>
         </div>
         {/* Button for mobile menu */}
       </div>
@@ -115,9 +193,29 @@ function Navbar() {
       >
         <div className="flex justify-between items-center px-4 py-3 border-b border-gray-300 dark:border-gray-700 text-center">
           {session ? (
-            <span className="block text-gray-200 py-1 font-bold">
-              Welcome, {user?.username || user?.email}
-            </span>
+            <div className="flex items-center gap-2">
+              <Avatar className="w-10 h-10 border  border-gray-300 dark:border-gray-700">
+                {avatar_uri ? (
+                  <>
+                    <AvatarImage
+                      src={avatar_uri}
+                      className="object-cover"
+                      alt="Profile Picture"
+                    />
+                    <AvatarFallback>
+                      {user?.username?.charAt(0) || "U"}
+                    </AvatarFallback>
+                  </>
+                ) : (
+                  <>
+                    <UserRound className="w-full h-full" />
+                  </>
+                )}
+              </Avatar>
+              <span className="block text-gray-200 py-1 font-bold">
+                Welcome, {user?.username || user?.email}
+              </span>
+            </div>
           ) : (
             <span className="text-lg font-bold text-gray-200 ">Welcome</span>
           )}
@@ -153,20 +251,34 @@ function Navbar() {
           </div>
         </div>
         {/* Menu Items */}
-        <div className="px-4 py-3 space-y-1">
+        <div className="px-4 py-3 space-y-1 flex flex-col gap-y-1">
           {session ? (
-            <Button
-              onClick={() => {
-                signOut();
-                setMobileMenuOpen(false);
-              }}
-              className="w-full  font-semibold bg-slate-100 text-gray-900 py-2 px-4 rounded mt-2"
-            >
-              Logout
-            </Button>
+            <>
+              <Link href="/profile/settings">
+                <Button className="w-full font-semibold transition-colors group bg-slate-100 text-gray-900 py-2 px-4 rounded hover:bg-gray-400">
+                  <Settings className="w-5 h-5 text-gray-800 group-hover:text-accent-foreground" />{" "}
+                  Profile Settings
+                </Button>
+              </Link>
+              <Link href="/dashboard">
+                <Button className="w-full font-semibold transition-colors group bg-slate-100 text-gray-900 py-2 px-4 rounded hover:bg-gray-400">
+                  <LayoutDashboard className="w-5 h-5 text-gray-800 group-hover:text-accent-foreground" />
+                  Dashboard
+                </Button>
+              </Link>
+              <Button
+                onClick={() => {
+                  signOut();
+                  setMobileMenuOpen(false);
+                }}
+                className="w-full font-semibold bg-slate-100 text-gray-900 py-2 px-4 rounded hover:bg-gray-400 mt-2"
+              >
+                Logout
+              </Button>
+            </>
           ) : (
             <Link href="/sign-in">
-              <Button className="w-full font-semibold bg-slate-100 text-gray-900 py-2 px-4 rounded mt-2">
+              <Button className="w-full font-semibold bg-slate-100 text-gray-900 py-2 px-4 rounded hover:bg-gray-400 mt-2">
                 Login
               </Button>
             </Link>
