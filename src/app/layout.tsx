1598e70fa7f35e5d7e4899ca3d7ca226b72afbc5
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2025-03-20 00:50:20 +0530

    Added Vercel Analytics, metadata with og image, favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,24 +5,67 @@ import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 import { APP_NAME } from "@/config/config";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"],
+  subsets: ["latin"]
 });
 
+const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
 export const metadata: Metadata = {
-  title: APP_NAME,
-  description: "Send anonymous messages",
+  title: `${APP_NAME} - Send anonymous messages`,
+  description:
+    "Send and receive anonymous messages effortlessly. Get honest feedback or fun secrets from friends.",
+  openGraph: {
+    title: `${APP_NAME} - Send anonymous messages`,
+    description:
+      "Send and receive anonymous messages effortlessly. Get honest feedback or fun secrets from friends.",
+
+    url: baseUrl,
+    siteName: `${APP_NAME} - Send anonymous messages`,
+    images: [
+      {
+        url: `${baseUrl}/images/og.png`,
+        width: 1200,
+        height: 630,
+        alt: `${APP_NAME} Preview Image`
+      }
+    ],
+    type: "website"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: `${APP_NAME} - Send anonymous messages`,
+    description:
+      "Send and receive anonymous messages effortlessly. Get honest feedback or fun secrets from friends.",
+    images: [`${baseUrl}/images/og.png`]
+  },
+  keywords: [
+    "anonymous messaging",
+    "send anonymous messages",
+    "honest feedback",
+    "ask me anything",
+    "anonymous chat"
+  ],
+  icons: {
+    icon: `${baseUrl}/favicon.svg`,
+    apple: `${baseUrl}/favicon.svg`
+  },
+  robots: "index, follow"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -40,6 +83,7 @@ export default function RootLayout({
           >
             {children}
             <Toaster />
+            <Analytics />
           </ThemeProvider>
         </body>
       </AuthProvider>
