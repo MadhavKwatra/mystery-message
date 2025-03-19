4ae93462435481ffcf9b104cd854c9f6556f6b6e
Madhav Kwatra <87752844+MadhavKwatra@users.noreply.github.com>
2025-03-20 01:39:09 +0530

    Fixing bug with ogImage url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 const baseUrl =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : `https://${process.env.VERCEL_URL}`;
+    : `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
 
 export const metadata: Metadata = {
   title: `${APP_NAME} - Send anonymous messages`,
